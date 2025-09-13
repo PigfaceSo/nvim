e9b36203d257df76f6421c9b7389dd8eeae5d3c7
PigfaceSo <112193655+PigfaceSo@users.noreply.github.com>
2025-09-13 23:31:56 +0700

    update

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -27,7 +27,8 @@ map("n", "<C-u>", "<C-u>zz", { desc = "Page Up" })
 -- map("n", "<leader>j", "<cmd>jumps<cr>", { desc = "Jumplist" })
 map("n", "J", "maJ`a<cmd>delmarks a<cr>", { desc = "Join", silent = true })
 map("n", "<esc>", "<cmd>nohls<cr><esc>", { desc = "No highlight (Normal)" })
-map("n", "<leader>y", '"+y', { desc = "Yank to clipboard" })
+map("n", "<leader>yy", '"+yy', { desc = "Yank line to clipboard" })
+map("n", "<leader>ya", 'gg"+yG<C-o>', { desc = "Yank all to clipboard" })
 
 -- Exit Insert mode
 map("i", "jj", "<esc>", { desc = "Exit insert mode" })
